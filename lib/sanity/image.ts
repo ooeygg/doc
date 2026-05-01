daa3d55166b1cba7698964e00c49b6d7c3a57aab
Michael <amichaelj1@gmail.com>
2026-05-01 15:19:34 -0400

    fix: handle missing sanity client by providing default projectId and dataset

--- lib/sanity/image.ts
+++ lib/sanity/image.ts
@@ -1,7 +1,9 @@
 import imageUrlBuilder, { type SanityImageSource } from "@sanity/image-url"
 import { sanityClient } from "lib/sanity"
 
-const builder = imageUrlBuilder(sanityClient)
+const builder = imageUrlBuilder(
+  sanityClient ?? { projectId: "placeholder", dataset: "production" }
+)
 
 export function urlFor(source: SanityImageSource) {
   return builder.image(source)
